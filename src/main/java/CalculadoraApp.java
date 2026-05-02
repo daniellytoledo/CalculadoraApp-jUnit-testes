408e84999caba3f2c0ab1d4ef211be5fb7405550
Dani <daniellytoledosouto@gmail.com>
2026-05-02 17:29:31 +0100

    @BeforeEach

--- src/main/java/CalculadoraApp.java
+++ src/main/java/CalculadoraApp.java
@@ -4,9 +4,15 @@ public class CalculadoraApp {
         return (a + b);
     }
 
+    public int divideInteiros(int a, int b) {
+        return (a / b);
+    }
+
     public static void main (String[] args) {
         CalculadoraApp calc = new CalculadoraApp();
         System.out.println(calc.somaInteiros(9, 2));
+
+        System.out.println(calc.divideInteiros(10, 2));
     }
 }
 
--- src/test/java/CalculadoraAppTest.java
+++ src/test/java/CalculadoraAppTest.java
@@ -1,16 +1,25 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-class CalculadoraAppTest {
-    @Test
-    void testSomaInteiros() {
-        // configuração
-        CalculadoraApp calc = new CalculadoraApp();
+public class CalculadoraAppTest {
 
-        // execução
-        int resultado = calc.somaInteiros(2,2);
-
-        // verificação
-        assertEquals(4, resultado, "Resultado Incorreto!");
+    CalculadoraApp calc;
+    @BeforeEach
+    void configura() {
+        calc = new CalculadoraApp();
     }
+
+    @Test
+    void testSoma() {
+        int resultado = calc.somaInteiros(3, 5);
+        assertEquals(8, resultado, "Resultado Incorreto");
+    }
+
+    @Test
+    void testDivide() {
+        int resultado = calc.divideInteiros(10, 5);
+        assertEquals(2, resultado, "A divisão está errada.");
+    }
+
 }
