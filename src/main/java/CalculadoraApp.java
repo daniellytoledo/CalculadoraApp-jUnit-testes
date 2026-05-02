716d760e42ee94d3a28238d217f3af32af7561ab
Dani <daniellytoledosouto@gmail.com>
2026-05-02 16:43:00 +0100

    public calculadora

--- src/main/java/CalculadoraApp.java
+++ src/main/java/CalculadoraApp.java
@@ -1,2 +1,12 @@
 public class CalculadoraApp {
+
+    public int somaInteiros(int a, int b) {
+        return (a + b);
+    }
+
+    public static void main (String[] args) {
+        CalculadoraApp calc = new CalculadoraApp();
+        System.out.println(calc.somaInteiros(8, 2));
+    }
 }
+
